74848a084dec7e6af92b0122a67d51bf551cda96
Nguyen Dang Minh <nguyendangminh@gmail.com>
2017-08-10 16:00:47 +0700

    First blood

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	var targetURL string
+	var port int
+	flag.StringVar(&targetURL, "u", "http://minhnd.com", "Target URL")
+	flag.IntVar(&port, "p", 1203, "Port to bind")
+	flag.Parse()
+
+	target, err := url.Parse(targetURL)
+	if err != nil {
+		log.Fatal("Parse URL failed: ", err)
+	}
+
+	proxy := NewReverseProxy(target)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), proxy))
+}
+
+func NewReverseProxy(target *url.URL) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+		req.Header.Set("Host", target.Host)
+		req.Host = target.Host
+		log.Println(req.Host)
+		log.Println(req.URL)
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
